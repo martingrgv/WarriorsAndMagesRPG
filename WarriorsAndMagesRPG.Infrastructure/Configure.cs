8eda5b32cae687b2bde85b7d100c139e53c332ff
Kristian Georgiev <krisi.geor12@gmail.com>
2024-07-16 04:14:27 +0300

    Configured empty dbcontext

---
+++ WarriorsAndMagesRPG.Infrastructure/Configure.cs
@@ -0,0 +1,7 @@
+﻿namespace WarriorsAndMagesRPG.Infrastructure
+{
+    internal static class Configure
+    {
+        public static string connectionString = "Server=localhost;Database=WarriorsAndMages;Trusted_Connection=True";
+    }
+}
---
+++ WarriorsAndMagesRPG.Infrastructure/WarriorsAndMagesContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WarriorsAndMagesRPG.Infrastructure
+{
+    public class WarriorsAndMagesContext : DbContext
+    {
+        public WarriorsAndMagesContext()
+        {
+            
+        }
+
+        public WarriorsAndMagesContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configure.connectionString);
+            }
+        }
+    }
+}
