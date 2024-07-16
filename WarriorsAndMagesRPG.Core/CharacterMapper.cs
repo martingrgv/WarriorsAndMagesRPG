001211711bc12ca007de32f26bda8861a5bffde9
Kristian Georgiev <krisi.geor12@gmail.com>
2024-07-16 14:50:00 +0300

    Created Log feature

---
+++ WarriorsAndMagesRPG.Core/CharacterMapper.cs
@@ -0,0 +1,45 @@
+﻿using WarriorsAndMagesRPG.Core.Models;
+using WarriorsAndMagesRPG.Infrastructure.Models;
+using WarriorsAndMagesRPG.Infrastructure.Models.Enums;
+
+namespace WarriorsAndMagesRPG.Core
+{
+    public class CharacterMapper
+    {
+        public static Character ToCharacter(CharacterViewModel characterViewModel)
+        {
+            var characterClass = GetCharacterClass(characterViewModel);
+
+            return new Character
+            {
+                CharacterClass = characterClass,
+                Health = characterViewModel.Health,
+                Strength = characterViewModel.Strength,
+                Agility = characterViewModel.Agility,
+                Intelligence = characterViewModel.Intelligence,
+                Damage = characterViewModel.Damage,
+                Range = characterViewModel.Range,
+                CharacterSymbol = characterViewModel.CharacterSymbol,
+                DateCreated = characterViewModel.DateCreated
+            };
+        }
+
+        private static CharacterClass GetCharacterClass(CharacterViewModel characterViewModel)
+        {
+            if (characterViewModel is Warrior)
+            {
+                return CharacterClass.Warrior;
+            }
+            else if (characterViewModel is Mage)
+            {
+                return CharacterClass.Mage;
+            }
+            else if (characterViewModel is Archer)
+            {
+                return CharacterClass.Archer;
+            }
+
+            throw new InvalidOperationException("Could not convert character class!");
+        }
+    }
+}
--- WarriorsAndMagesRPG.Core/Contracts/IController.cs
+++ WarriorsAndMagesRPG.Core/Contracts/IController.cs
@@ -1,10 +1,12 @@
 ﻿using WarriorsAndMagesRPG.Core.Models;
+using WarriorsAndMagesRPG.Infrastructure;
 
 namespace WarriorsAndMagesRPG.Core.Contracts
 {
     public interface IController
     {
-        Character GetCharacter(char choice);
-        int AddStatsToCharacter(Character character, string statName, int points);
+        CharacterViewModel GetCharacter(char choice);
+        int AddStatsToCharacter(CharacterViewModel character, string statName, int points);
+        void LogCharacter(CharacterViewModel characterViewModel);
     }
 }
